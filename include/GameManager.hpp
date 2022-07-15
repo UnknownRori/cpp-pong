f7ba38f69114d61848d06b1ff0c1f7fd29adc164
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-07-15 08:40:59 +0700

    🔨 Another redesign on GameManager.cpp

--- include/GameManager.hpp
+++ include/GameManager.hpp
@@ -3,51 +3,53 @@
 
 #include <array>
 #include "SFML/Graphics.hpp"
-#include "Entity.hpp"
+#include "Ball.hpp"
+#include "Paddle.hpp"
 #include "Score.hpp"
 
 namespace Pong
 {
     class GameManager
     {
-        private:
-            // SFML Event Poll
-            sf::Event m_event;
+    private:
+        // SFML Event Poll
+        sf::Event m_event;
 
-            // SFML Window
-            sf::RenderWindow m_window;
+        // SFML Window
+        sf::RenderWindow m_window;
 
-            // This is where to store the entity
-            std::array<Pong::Entity, 3> m_entityPoll;
+        // This is where to store the entity
+        std::array<Pong::Paddle, 2> m_paddle;
+        Pong::Ball m_ball;
 
-            // This is where to store the score
-            Pong::Score score;
+        // This is where to store the score
+        Pong::Score score;
 
-        public:
-            // ¯\_(ツ)_/¯
-            GameManager();
+    public:
+        // ¯\_(ツ)_/¯
+        GameManager();
 
-            // Do some housekeeping
-            ~GameManager();
+        // Do some housekeeping
+        ~GameManager();
 
-            // Initialize the window
-            void init(std::string &title, sf::Vector2u &screenSize);
+        // Initialize the window
+        void init(std::string &title, sf::Vector2u &screenSize);
 
-        private:
-            // Main Game loop
-            void loop();
+    private:
+        // Main Game loop
+        void loop();
 
-            // This is where to define the control
-            void eventPollHandler();
+        // This is where to define the control
+        void eventPollHandler();
 
-            // This is where to draw the entity
-            void screenUpdateHandler(Pong::Entity &entity);
+        // This is where to draw the entity
+        void screenUpdateHandler(Pong::Entity &entity);
 
-            // This is where to run update method on entity
-            void updateHandler(Pong::Entity &entity);
+        // This is where to run update method on entity
+        void updateHandler(Pong::Entity &entity);
 
-            // This is for reseting the game
-            void reset();
+        // This is for reseting the game
+        void reset();
     };
 }
 
