257c1eb4e2955ba18924708db9fb786599e8988f
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-07-16 09:17:03 +0700

    🔨 Fix some object property name

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -12,8 +12,8 @@ namespace Pong
     class Entity
     {
     protected:
-        sf::Vector2f currentSpeed = sf::Vector2f(0, 0);
-        sf::Vector2f baseSpeed = sf::Vector2f(0, 0);
+        sf::Vector2f m_currentSpeed = sf::Vector2f(0, 0);
+        sf::Vector2f m_baseSpeed = sf::Vector2f(0, 0);
 
     public:
         // ¯\_(ツ)_/¯
@@ -28,8 +28,6 @@ namespace Pong
         virtual void reset();
 
         virtual void setPos(const sf::Vector2f &newPosition);
-
-        virtual void move();
     };
 }
 
