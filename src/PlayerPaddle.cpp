06e8075c78a2901d0ba113cc45701f05644975cf
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-07-16 09:29:10 +0700

    🔨 Implement user input to move the playable paddle

--- src/PlayerPaddle.cpp
+++ src/PlayerPaddle.cpp
@@ -7,5 +7,12 @@ Pong::PlayerPaddle::~PlayerPaddle() {}
 
 void Pong::PlayerPaddle::update(const sf::Vector2u &screenSize, const sf::Event &event)
 {
-    //
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+        this->move(screenSize, this->m_baseSpeed);
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+        this->move(screenSize, sf::Vector2f(0, -this->m_baseSpeed.y));
+    }
 }
